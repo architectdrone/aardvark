ffbd6e838b6139534138b72212e5009a63e06a76
EvanBrown96 <evanbrown@ku.edu>
2019-09-20 16:06:08 -0500

    added eeprom state saving
    this will allow the voltage to be maintained across resets

--- aardvark_code.ino
+++ aardvark_code.ino
@@ -3,6 +3,13 @@
  * Made for JEDL Project 1
  */
 
+#include <EEPROM.h>
+
+/**
+ * location to store voltage state info in
+ */
+const int STATE_ADDR = 34;
+
 /**
  * pin definitions
  */
@@ -72,8 +79,16 @@ void setup() {
    */
   Serial.begin(9600); // starts the serial (over USB) connection
   pinMode(PWM_PIN, OUTPUT);
+  
+  // calibrate arduino based on 3.3V pin
   calibrate();
-  setDesired(8.0);
+  
+  // read saved voltage state from EEPROM and set initial voltage
+  float desired = static_cast<float>(EEPROM.read(STATE_ADDR))/2;
+  if(8 <= desired && desired <= 12) setDesired(desired);
+  else                              setDesired(8.0);
+  
+  // setup voltage/current averaging tables
   for(int i = 1; i < READ_COUNT; i++){
     v_reads[i] = 0;
     i_reads[i] = 0;
@@ -91,6 +106,7 @@ void setDesired(float voltage){
    
   desired = voltage;
   pwm_out = PWM_MIN + ((desired - V_MIN) * (PWM_MAX - PWM_MIN)) / (V_MAX - V_MIN);
+  EEPROM.update(STATE_ADDR, static_cast<byte>(desired*2));
   
 }
 
